b6741ea1e2e9d220128e3debddfb6eb40e1a963d
ruby149 <rubiselbenitez30@gmail.com>
2020-07-09 11:24:26 -0400

    final

--- factoryApp/Program.cs
+++ factoryApp/Program.cs
@@ -11,8 +11,9 @@ namespace factoryApp
 
             do
             {
-                Console.WriteLine("Enter the pokemonnumber of the Pokemon you want to train");
-                 input = int.TryParse(Console.ReadLine(), out pokemonNumber);
+                Console.WriteLine("Enter the pokemon number of the Pokemon you want to train");
+                Console.WriteLine("Based on your number the pokemon you are going to train will appear and a few facts will also appear.");
+                input = int.TryParse(Console.ReadLine(), out pokemonNumber);
             }
             while (!input);
             var Pokemon =Training.GetPokemon(pokemonNumber);
